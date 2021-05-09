2e94ceea3f29e5806e1256e8fba370871bd6f37b
Evandro Araujo <64655153+Evandromck@users.noreply.github.com>
2021-05-09 18:21:16 -0300

    Add files via upload

---
+++ for_contagem.py
@@ -0,0 +1,5 @@
+from time import sleep
+for n in range(0, 50, 2):
+        sleep(0.2)
+        print(n)
+print('acabou')
---
+++ range.py
@@ -0,0 +1,3 @@
+for c in range(0, 8):# (c) é a variavel que vc escolhe! (0,8) a contagem é de 0 ate 8. vai ate 7
+     print(c)
+print('Acabou')
